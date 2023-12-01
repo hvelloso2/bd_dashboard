afcc3cf13585b87a513766b8d146c3b06b9232b3
Henrique Velloso <100692309+hvelloso2@users.noreply.github.com>
2023-11-30 23:56:07 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -39,6 +39,7 @@ CREATE TABLE Occurrence (
 );
 
 
+
 CREATE TABLE Kingdom (
     kingdom_id SERIAL PRIMARY KEY,
     kingdom TEXT
@@ -84,3 +85,43 @@ CREATE TABLE OccurrenceTaxonomy (
     taxonid INTEGER REFERENCES Taxonomy(id),
     PRIMARY KEY (id, taxonid)
 );
+
+-- Create Log table
+CREATE TABLE OccurrenceLog (
+    log_id SERIAL PRIMARY KEY,
+    event_type TEXT, -- Insert, Update, Delete
+    event_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER, -- You may need to adjust this based on your user management system
+    table_name TEXT,
+    record_id INTEGER,
+    new_data JSONB, -- Store new data for updates or inserted data
+    old_data JSONB -- Store old data for updates or deleted data
+);
+
+-- Trigger function for logging
+CREATE OR REPLACE FUNCTION log_occurrence_changes()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO OccurrenceLog (event_type, user_id, table_name, record_id, new_data)
+        VALUES ('Insert', CURRENT_USER, TG_TABLE_NAME, NEW.id, TO_JSONB(NEW));
+        RETURN NEW;
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO OccurrenceLog (event_type, user_id, table_name, record_id, new_data, old_data)
+        VALUES ('Update', CURRENT_USER, TG_TABLE_NAME, NEW.id, TO_JSONB(NEW), TO_JSONB(OLD));
+        RETURN NEW;
+    ELSIF TG_OP = 'DELETE' THEN
+        INSERT INTO OccurrenceLog (event_type, user_id, table_name, record_id, old_data)
+        VALUES ('Delete', CURRENT_USER, TG_TABLE_NAME, OLD.id, TO_JSONB(OLD));
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger to call the log_occurrence_changes function
+CREATE TRIGGER log_occurrence_changes_trigger
+AFTER INSERT OR UPDATE OR DELETE ON Occurrence
+FOR EACH ROW
+EXECUTE FUNCTION log_occurrence_changes();
+
